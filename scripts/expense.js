718bbdd536f713bef2cf2f2bc54c074e532f4226
reemlifa <rkhalifa50@stuy.edu>
2025-08-13 20:57:21 +0100

    erm tried to learn how to take multiple inputs and put them into tds, didn't work

--- scripts/expense.js
+++ scripts/expense.js
@@ -4,16 +4,23 @@ function addRow() {
   let date = document.getElementById("date").value;
   let text = document.getElementById("text").value;
   let amount = document.getElementById("amount").value;
+  let button = document.getElementById("delete");
+  button.innerHTML = "X";
 
   let tableBody = document.querySelector("#table");
 
-  const HTML = `
-    <tr>
-      <td>${date}</td>
-      <td>${text}</td>
-      <td>${amount}</td>
-    </tr>
-`;
+  let tableRow = document.createElement("tr");
 
-  tableBody.insertAdjacentHTML("beforeend", HTML);
+  let dateCell = document.createElement("td");
+  let textCell = document.createElement("td");
+  let amountCell = document.createElement("td");
+  let deleteCell = document.createElement("td");
+
+  tableRow.appendChild(dateCell);
+  tableRow.appendChild(textCell);
+  tableRow.appendChild(amountCell);
+
+  button.addEventListener("click", function () {
+    tr.remove();
+  });
 }
